8674e3d7047be92702db472c23ad5ccdb54a3454
pde-bakk <pde-bakk@student.codam.nl>
2023-01-17 20:09:20 +0100

    adding some flaggies

--- include/flags.h
+++ include/flags.h
@@ -10,6 +10,12 @@
 #define FLAG_a	(1 << 2)
 #define FLAG_r	(1 << 3)
 #define FLAG_t	(1 << 4)
+#define FLAG_d	(1 << 5)
+#define FLAG_f	(1 << 6)
+#define FLAG_g	(1 << 7)
+#define FLAG_G	(1 << 8)
+#define FLAG_h	(1 << 9)
+#define FLAG_u	(1 << 10)
 
 extern unsigned int g_flags;
 
